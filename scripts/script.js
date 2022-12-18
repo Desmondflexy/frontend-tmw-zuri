efc7dc3a24adca040356413cde1aedcda4269de4
Desmondflexy <ikilodesmond@gmail.com>
2022-12-18 13:17:33 +0100

    first commit

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,6 @@
+const menuItems = document.querySelector('.menu-block');
+const navBtn = document.querySelector('#nav-btn');
+
 window.onscroll = ()=>{
     const heading = document.querySelector('header');
     if (window.scrollY > 0){
@@ -6,3 +9,13 @@ window.onscroll = ()=>{
         heading.style.boxShadow = 'none';
     }
 }
+
+navBtn.addEventListener('click', () => {
+    if (navBtn.getAttribute('alt') === 'show menu') {
+            menuItems.style.display = 'block';
+        navBtn.setAttribute('alt', 'hide menu');
+    } else {
+            menuItems.style.display = 'none';
+        navBtn.setAttribute('alt', 'show menu');
+    }
+})
